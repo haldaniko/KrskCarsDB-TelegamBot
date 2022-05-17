1e01f6f8f7028423bb87079f3b5899e5b539e76d
Asus <Haldaniko@gmail.com>
2022-05-17 18:16:08 +0300

    Ver 0.2

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ bot = telebot.TeleBot(config.token)
 
 def db_car_number(message):
     if message.content_type == 'text' and 7 < len(message.text) < 10:
+        startkboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        catalog = telebot.types.KeyboardButton(text="Сделать запись")
+        info = telebot.types.KeyboardButton(text="Отмена")
+        startkboard.add(catalog, info)
         global car_number
         car_number = message.text.upper()
-        bot.send_message(message.from_user.id, "Прикрепите фотографию автомобиля с буквой Z.")
+        bot.send_message(message.from_user.id, "Прикрепите фотографию автомобиля с буквой Z.", reply_markup=startkboard)
         bot.register_next_step_handler(message, db_car_photo)
     elif message.content_type == 'text' and message.text.lower() == "отмена":
         bot.send_message(message.from_user.id, "Ввод отменен")
@@ -29,7 +33,7 @@ def db_car_photo(message):
         with sqlite3.connect("KRSK.db") as con:
             binary = sqlite3.Binary(downloaded_file)
             cur = con.cursor()
-            cur.execute("INSERT INTO cars (car_number, photo) VALUES (?, ?)", (car_number, binary))
+            cur.execute("INSERT INTO cars (car_number, photo, date) VALUES (?, ?, DATE())", (car_number, binary))
         bot.send_message(message.from_user.id, "Номер и фото успешно добавлены в таблицу")
     elif message.content_type == 'text' and message.text.lower() == "отмена":
         bot.send_message(message.from_user.id, "Ввод отменен")
@@ -40,16 +44,23 @@ def db_car_photo(message):
         bot.register_next_step_handler(message, db_car_photo)
 
 
+@bot.message_handler(commands=['start'])
+def keyboard_start(message):
+    startkboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    catalog = telebot.types.KeyboardButton(text="Сделать запись")
+    info = telebot.types.KeyboardButton(text="Помощь")
+    startkboard.add(catalog, info)
+    bot.send_message(message.chat.id,
+                     "Привет! Я предназначен для сбора данных об автовладельцах, поддерживающих войну в Украине. "
+                     "Чтобы открыть справку, введите команду \n\n/help",
+                     reply_markup=startkboard)
+
+
 @bot.message_handler(content_types=['text'])
 def messages(message):
-    if message.text.lower() == "/start":
-        bot.send_message(message.from_user.id, "Привет! Я предназначен для сбора данных об автовладельцах, "
-                                               "поддерживающих войну в Украине. Чтобы открыть справку, введите команду "
-                                               "/help")
-    elif message.text.lower() == "/help":
+    if message.text.lower() == "/help" or message.text.lower() == "помощь":
         bot.send_message(message.from_user.id, 'Доступные команды: \n🔸 /start'
-                                               '\n🔸 /help'
-                                               '\n🔸 Сделать запись')
+                                               '\n🔸 /help')
     elif message.text.lower() == "сделать запись":
         bot.send_photo(message.from_user.id, photo='https://upload.wikimedia.org/wikipedia/commons/thumb/1/14'
                                                    '/License_plate_in_Russia_2.svg/1920px-License_plate_in_Russia_2'
